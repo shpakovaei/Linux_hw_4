5e9cb1c45633a19d36a1ce594240918d2476f960
shpakovaei <113781605+shpakovaei@users.noreply.github.com>
2023-09-30 22:05:38 +0600

    Create cheks.py

---
+++ cheks.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def func(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+
+    if text in result.stdout and result.returncode == 0:
+        return True
+    else:
+        return False
+
+def getout(cmd):
+    return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
